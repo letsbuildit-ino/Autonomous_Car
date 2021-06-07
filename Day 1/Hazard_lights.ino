84a940d5736b4eb849773615bf779de708a356ee
LetsBuildIt <muhammadaqibhussain01@gmail.com>
2021-06-07 17:08:04 +0500

    Add files via upload

---
+++ Day 1/Hazard_lights.ino
@@ -0,0 +1,16 @@
+void setup() {
+  
+  pinMode(LED_BUILTIN, OUTPUT); // led builtin
+  pinMode(12, OUTPUT); // headlight leds
+  pinMode(13, OUTPUT); // piezo buzzer
+}
+
+void loop() {
+  
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(12, HIGH);
+  delay(500);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(12, LOW);
+  delay(500);                       // wait for a second
+}
